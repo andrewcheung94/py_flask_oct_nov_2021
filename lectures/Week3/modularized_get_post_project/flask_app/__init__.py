c7795725cd9a49947a6ed0c85b1641c8422a3e1c
unknown <pabarnard@outlook.com>
2021-10-14 15:24:22 -0700

    Added comment to init file

--- lectures/Week3/modularized_get_post_project/flask_app/__init__.py
+++ lectures/Week3/modularized_get_post_project/flask_app/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 import os
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY") # Whenever you use session, you MUST use a secret key
-print(app.secret_key)
+# print(app.secret_key) # Just for debugging - don't actually print this in production!
